92fdd9e5d8d5480da8c7630a5dc157454b72cde9
GOHAN28jk <84548918+GOHAN28jk@users.noreply.github.com>
2021-06-07 19:56:52 -0500

    Add files via upload

---
+++ ventaMVC/global/config.php
@@ -0,0 +1,12 @@
+<?php
+    define("SERVIDOR", "localhost");
+    define("USUARIO", "admin");
+    define("PASSWORD", "johan123");
+    define("BD", "facturacion");
+
+    define("KEY", "istvidanueva123");
+    define('COD', 'AES-128-ECB');
+    define('LENG'.'es');
+
+   
+?>
